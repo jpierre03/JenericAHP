8ace869dbd91e69f324f45d39a190766234ee1b8
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2010-03-07 20:03:48 +0000

    MaJ de la licence (2009-> 2009-2010) & nom JenericAHP
    git-svn-id: http://svn.dubronetwork.fr/genericahp/trunk@51 454fbc2d-86d7-48e1-a163-88cb0aa343ab

--- GenericAHP/src/org/taeradan/ahp/Alternative.java
+++ GenericAHP/src/org/taeradan/ahp/Alternative.java
@@ -1,19 +1,19 @@
-/* Copyright 2009 Yves Dubromelle @ LSIS(www.lsis.org)
+/* Copyright 2009-2010 Yves Dubromelle @ LSIS(www.lsis.org)
  *
- * This file is part of GenericAHP.
+ * This file is part of JenericAHP.
  *
- * GenericAHP is free software: you can redistribute it and/or modify
+ * JenericAHP is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * GenericAHP is distributed in the hope that it will be useful,
+ * JenericAHP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GenericAHP.  If not, see <http://www.gnu.org/licenses/>.
+ * along with JenericAHP.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.taeradan.ahp;
 
