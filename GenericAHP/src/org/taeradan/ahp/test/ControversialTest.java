051bcba3b31c989955f71ef7b38799e9a88c792c
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2010-07-02 16:00:52 +0000

    fix: chemin des fichiers de configuration
    + correction de l'appel des fichiers de configuration
    	chemins codés en dur --> appel à getRessource()
    
    git-svn-id: http://svn.dubronetwork.fr/genericahp/trunk@96 454fbc2d-86d7-48e1-a163-88cb0aa343ab

--- GenericAHP/src/org/taeradan/ahp/test/ControversialTest.java
+++ GenericAHP/src/org/taeradan/ahp/test/ControversialTest.java
@@ -18,11 +18,12 @@
 package org.taeradan.ahp.test;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import org.taeradan.ahp.Root;
 
 /**
- *
+ * @author jpierre03
  * @author Yves Dubromelle
  */
 public class ControversialTest {
@@ -44,38 +45,58 @@ public class ControversialTest {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		final Root root = new Root(new File(
-				"build/classes/org/taeradan/ahp/conf/controversial_test.xml"),
+		URL resource = TestAhp.class.getResource(
+				"/org/taeradan/ahp/conf/controversial_test.xml");
+		File aFile = new File(resource.getFile());
+
+		final Root root = new Root(aFile,
 								   Root.indicatorPath);
 		System.out.println(root.toStringRecursive());
-		final ArrayList<ControversialAlternative> alts = new ArrayList<ControversialAlternative>(4);
-		alts.add(new ControversialAlternative("Alternative 1", (1 / 5.) * TEMOIN_MAX, ((1 / 5.)
-																					   * VARIABLE_MAX)
-																					  + DECALAGE));
-		System.out.println("Alternative 1, valueIT=" + (1 / 5.) * TEMOIN_MAX + ", valueIV="
-						   + (((1 / 5.) * VARIABLE_MAX) + DECALAGE));
-		alts.add(new ControversialAlternative("Alternative 2", (2 / 5.) * TEMOIN_MAX, ((2 / 5.)
-																					   * VARIABLE_MAX)
-																					  + DECALAGE));
-		System.out.println("Alternative 2, valueIT=" + (2 / 5.) * TEMOIN_MAX + ", valueIV="
-						   + (((2 / 5.) * VARIABLE_MAX) + DECALAGE));
-		alts.add(new ControversialAlternative("Alternative 3", (3 / 5.) * TEMOIN_MAX, ((3 / 5.)
-																					   * VARIABLE_MAX)
-																					  + DECALAGE));
-		System.out.println("Alternative 3, valueIT=" + (3 / 5.) * TEMOIN_MAX + ", valueIV="
-						   + (((3 / 5.) * VARIABLE_MAX) + DECALAGE));
-		alts.add(new ControversialAlternative("Alternative 4", (4 / 5.) * TEMOIN_MAX, ((4 / 5.)
-																					   * VARIABLE_MAX)
-																					  + DECALAGE));
-		System.out.println("Alternative 4, valueIT=" + (4 / 5.) * TEMOIN_MAX + ", valueIV="
-						   + (((4 / 5.) * VARIABLE_MAX) + DECALAGE));
-		alts.add(new ControversialAlternative("Alternative 5", (5 / 5.) * TEMOIN_MAX, ((5 / 5.)
-																					   * VARIABLE_MAX)
-																					  + DECALAGE));
-		System.out.println("Alternative 5, valueIT=" + (5 / 5.) * TEMOIN_MAX + ", valueIV="
-						   + (((5 / 5.) * VARIABLE_MAX) + DECALAGE));
+		final ArrayList<ControversialAlternative> alts = new ArrayList<ControversialAlternative>(
+				4);
+		alts.add(new ControversialAlternative("Alternative 1", ( 1 / 5. )
+															   * TEMOIN_MAX,
+											  ( ( 1 / 5. )
+												* VARIABLE_MAX )
+											  + DECALAGE));
+		System.out.println("Alternative 1, valueIT=" + ( 1 / 5. ) * TEMOIN_MAX
+						   + ", valueIV="
+						   + ( ( ( 1 / 5. ) * VARIABLE_MAX ) + DECALAGE ));
+		alts.add(new ControversialAlternative("Alternative 2", ( 2 / 5. )
+															   * TEMOIN_MAX,
+											  ( ( 2 / 5. )
+												* VARIABLE_MAX )
+											  + DECALAGE));
+		System.out.println("Alternative 2, valueIT=" + ( 2 / 5. ) * TEMOIN_MAX
+						   + ", valueIV="
+						   + ( ( ( 2 / 5. ) * VARIABLE_MAX ) + DECALAGE ));
+		alts.add(new ControversialAlternative("Alternative 3", ( 3 / 5. )
+															   * TEMOIN_MAX,
+											  ( ( 3 / 5. )
+												* VARIABLE_MAX )
+											  + DECALAGE));
+		System.out.println("Alternative 3, valueIT=" + ( 3 / 5. ) * TEMOIN_MAX
+						   + ", valueIV="
+						   + ( ( ( 3 / 5. ) * VARIABLE_MAX ) + DECALAGE ));
+		alts.add(new ControversialAlternative("Alternative 4", ( 4 / 5. )
+															   * TEMOIN_MAX,
+											  ( ( 4 / 5. )
+												* VARIABLE_MAX )
+											  + DECALAGE));
+		System.out.println("Alternative 4, valueIT=" + ( 4 / 5. ) * TEMOIN_MAX
+						   + ", valueIV="
+						   + ( ( ( 4 / 5. ) * VARIABLE_MAX ) + DECALAGE ));
+		alts.add(new ControversialAlternative("Alternative 5", ( 5 / 5. )
+															   * TEMOIN_MAX,
+											  ( ( 5 / 5. )
+												* VARIABLE_MAX )
+											  + DECALAGE));
+		System.out.println("Alternative 5, valueIT=" + ( 5 / 5. ) * TEMOIN_MAX
+						   + ", valueIV="
+						   + ( ( ( 5 / 5. ) * VARIABLE_MAX ) + DECALAGE ));
 		root.calculateRanking(alts);
-		System.out.println("======================================================");
+		System.out.println(
+				"======================================================");
 		System.out.println(root.resultToString());
 		System.out.println("Valeurs de \"rank\" pour chaque alternative:");
 		for (ControversialAlternative currentAlt : alts) {
--- GenericAHP/src/org/taeradan/ahp/test/TestAhp.java
+++ GenericAHP/src/org/taeradan/ahp/test/TestAhp.java
@@ -18,47 +18,54 @@
 package org.taeradan.ahp.test;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
 import org.taeradan.ahp.Root;
 
 /**
  * Test class for the AHP tree
+ * @author jpierre03
  * @author Yves Dubromelle
  */
 public class TestAhp {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		final Root root = new Root(new File("build/classes/org/taeradan/ahp/conf/ahp_conf.xml"),
-								   Root.indicatorPath);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        URL resource = TestAhp.class.getResource("/org/taeradan/ahp/conf/ahp_conf.xml");
+        File aFile = new File(resource.getFile());
+
+        final Root root = new Root(aFile,
+                Root.indicatorPath);
 //		System.out.println(root.toStringRecursive());
-		final ArrayList<TestingAlternative> alts = new ArrayList<TestingAlternative>(4);
-		for (int index = 0; index < 2; index++) {
-			alts.add(new TestingAlternative("Alternative 1", 3, 3, 3, 3, 3, 3, 3, 3, 3));
-			alts.add(new TestingAlternative("Alternative 2", 1, 1, 1, 1, 1, 1, 1, 1, 1));
-			alts.add(new TestingAlternative("Alternative 3", 4, 4, 4, 4, 4, 4, 4, 4, 4));
-			alts.add(new TestingAlternative("Alternative 4", 2, 2, 2, 2, 2, 2, 2, 2, 2));
-			alts.add(new TestingAlternative("Alternative 5", 5, 5, 5, 5, 5, 5, 5, 5, 5));
-		}
+        final ArrayList<TestingAlternative> alts = new ArrayList<TestingAlternative>(4);
+        for (int index = 0; index < 2; index++) {
+            alts.add(new TestingAlternative("Alternative 1-"+index, 3, 3, 3, 3, 3, 3, 3, 3, 3));
+            alts.add(new TestingAlternative("Alternative 2-"+index, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+            alts.add(new TestingAlternative("Alternative 3-"+index, 4, 4, 4, 4, 4, 4, 4, 4, 4));
+            alts.add(new TestingAlternative("Alternative 4-"+index, 2, 2, 2, 2, 2, 2, 2, 2, 2));
+            alts.add(new TestingAlternative("Alternative 5-"+index, 5, 5, 5, 5, 5, 5, 5, 5, 5));
+        }
 //		alts.add(new TestingAlternative("Alternative 1", 3, 5, 3, 1 / 2., 3, 1200, 1, 100, 1));
 //		alts.add(new TestingAlternative("Alternative 2", 3, 5, 3, 1 / 2., 1, 8638, 1, 250, 1));
 //		alts.add(new TestingAlternative("Alternative 3", 5, 5, 5, 2 / 3., 3, 1800, 1, 30, 1));
 //		alts.add(new TestingAlternative("Alternative 4", 3, 3, 3, 3 / 4., 3, 7136, 1, 300, 1));
-		System.out.println("Calculate Ranking =============");
-		root.calculateRanking(alts);
+        System.out.println("Calculate Ranking =============");
+        root.calculateRanking(alts);
 
 //		alts.add(new TestingAlternative("Alternative "+Math.random(), Math.random(),Math.random(),Math.random(),Math.random(),Math.random(), Math.random(), Math.random(), Math.random(), Math.random()));
 //		alts.add(new TestingAlternative("Alternative "+Math.random(), Math.random(),Math.random(),Math.random(),Math.random(),Math.random(), Math.random(), Math.random(), Math.random(), Math.random()));
 //		root.calculateRanking(alts);
-		System.out.println(
-				"======================================================");
+        System.out.println(
+                "======================================================");
 //		System.out.println(root.resultToString());
-		System.out.println("Valeurs de \"rank\" pour chaque alternative:");
-		for (TestingAlternative currentAlt : alts) {
-			System.out.println(currentAlt.name + " = " + currentAlt.getRank());
-		}
-	}
+        System.out.println("Valeurs de \"rank\" pour chaque alternative:");
+        for (TestingAlternative currentAlt : alts) {
+            System.out.println(currentAlt.name + " = " + currentAlt.getRank());
+        }
+    }
+
+    private TestAhp() {
+    }
 }
