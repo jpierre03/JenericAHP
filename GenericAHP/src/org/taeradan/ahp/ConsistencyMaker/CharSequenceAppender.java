e06cc3ead578718214363043d27dc2c010f4f279
Marianne MAZADE <marianne.mazade@gmail.com>
2011-04-07 08:28:53 +0000

    (Comitt de test) & commentaires d'une classe.
    git-svn-id: http://svn.dubronetwork.fr/genericahp/trunk@121 454fbc2d-86d7-48e1-a163-88cb0aa343ab

--- GenericAHP/src/org/taeradan/ahp/ConsistencyMaker/CharSequenceAppender.java
+++ GenericAHP/src/org/taeradan/ahp/ConsistencyMaker/CharSequenceAppender.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 
 /**
  * @author Marianne
+ * This class allows to insert separators (used in csv files) or datas in a file
  */
 public class CharSequenceAppender {
 
@@ -16,18 +17,30 @@ public class CharSequenceAppender {
 		output = new PrintWriter(new BufferedWriter(new FileWriter(nomFichier, true)));
 	}
 
+	/*
+	 * Write an object in the opened file
+	 */
 	public void append(CharSequence object) {
 		output.append(object);
 	}
 
+	/*
+	 * Write a linefeed in the opened file
+	 */
 	public void insertLineFeed() {
 		output.append("\n");
 	}
 
+	/*
+	 * Write a separator separator in the opened file
+	 */
 	public void insertSeparator() {
 		output.append(";");
 	}
 
+	/*
+	 * Write a matrix in the opened file
+	 */
 	public void insertMatrix(MyMatrix myMatrix) {
 		String chaine = "";
 
