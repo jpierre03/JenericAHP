0f58142d571b52f54e8a16e9de692ba60d9b4f08
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2013-01-20 18:37:47 +0100

    test unitaire simple pour MatrixValue

--- GenericAHP/test/org/taeradan/ahp/AHP_TestSuite.java
+++ GenericAHP/test/org/taeradan/ahp/AHP_TestSuite.java
@@ -4,9 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.taeradan.ahp.consistency.ConsistencyTest;
 import org.taeradan.ahp.controversial.ControversialTest;
+import org.taeradan.ahp.matrix.MatrixValueTest;
 
 /** @author Jean-Pierre PRUNARET */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ControversialTest.class, ConsistencyTest.class})
+@Suite.SuiteClasses({ConsistencyTest.class,
+					 ControversialTest.class,
+					 MatrixValueTest.class})
 public class AHP_TestSuite {
 }
