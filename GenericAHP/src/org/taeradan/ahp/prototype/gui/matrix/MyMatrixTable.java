5703a69db0647dbb2b0ec779cd64b3a9c2237aef
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2013-01-28 01:56:43 +0100

    unused Netbeans generated GUI forms

--- GenericAHP/src/org/taeradan/ahp/prototype/gui/matrix/MyMatrixTable.java
+++ GenericAHP/src/org/taeradan/ahp/prototype/gui/matrix/MyMatrixTable.java
@@ -10,10 +10,9 @@ import javax.swing.JTable;
  * @author Jean-Pierre PRUNARET
  * @author Marianne
  */
-public class MyMatrixTable
+public final class MyMatrixTable
 		extends JTable {
 
 	public static final int ROWS   = 5;
 	public static final int COLUMN = 5;
-
 }
