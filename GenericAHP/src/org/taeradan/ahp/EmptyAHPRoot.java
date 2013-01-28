4422b85adf72f520e5a97d8df2470c47d96e164f
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2013-01-28 01:46:52 +0100

    create an AHP Tree template with 3 criteria

---
+++ GenericAHP/src/org/taeradan/ahp/EmptyAHPRoot.java
@@ -0,0 +1,12 @@
+package org.taeradan.ahp;
+
+import java.io.File;
+
+public class EmptyAHPRoot
+		extends AHPRoot {
+
+	public EmptyAHPRoot() {
+		super(new File(EmptyAHPRoot.class.getResource("/org/taeradan/ahp/conf/empty_ahp.xml").getFile()),
+			  AHPRoot.DEFAULT_INDICATOR_PATH);
+	}
+}
