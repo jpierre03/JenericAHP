65263fa90cb66d3727b995a39ca0aed6e0e56d19
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2013-03-23 20:15:59 +0100

    mark class with xml serializer

---
+++ GenericAHP/src/org/taeradan/ahp/XmlOutputable.java
@@ -0,0 +1,11 @@
+package org.taeradan.ahp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jpierre03
+ * Date: 23/03/13
+ * Time: 20:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class XmlOutputable {
+}
