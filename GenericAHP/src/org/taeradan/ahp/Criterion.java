632d265bb4297efa69ed69aa94b77c22f6b17383
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2013-10-06 12:15:08 +0200

    comment

--- GenericAHP/src/org/taeradan/ahp/Criterion.java
+++ GenericAHP/src/org/taeradan/ahp/Criterion.java
@@ -35,14 +35,14 @@ import java.util.logging.Logger;
  * @author Yves Dubromelle
  */
 public class Criterion
-	implements XmlOutputable{
+	implements XmlOutputable {
 
 	//	AHP configuration attributes
-	private final PriorityVector        vectorIndicatorCriteria;
+	private final PriorityVector vectorIndicatorCriteria;
 	private final Collection<Indicator> indicators;
-	private       String                identifier;
-	private       String                name;
-	private       PairWiseMatrix        matrixIndicatorIndicator;
+	private String identifier;
+	private String name;
+	private PairWiseMatrix matrixIndicatorIndicator;
 
 	//	AHP execution attributes
 	private final ConsistencyChecker consistencyChecker = new ConsistencyChecker();
@@ -54,53 +54,53 @@ public class Criterion
 	 * @param xmlCriteria
 	 */
 	public Criterion(final Element xmlCriteria) {
-//		Initialisation of the id of the criteria
+		/** Initialisation of the id of the criteria */
 		identifier = xmlCriteria.getAttributeValue("id");
 //		System.out.println("\tCriterion.id="+id);
 
-//		Initialisation of the name
+		/** Initialisation of the name */
 		name = xmlCriteria.getChildText("name");
 //		System.out.println("\tCriterion.name="+name);
 
-//		Initialisation of the preference matrix
+		/** Initialisation of the preference matrix */
 		final Element xmlPrefMatrix = xmlCriteria.getChild("prefmatrix");
 		matrixIndicatorIndicator = PairWiseMatrix.builder(xmlPrefMatrix);
 //		System.out.println("\tCriterion.matrixIndicatorIndicator="+matrixIndicatorIndicator);
 		vectorIndicatorCriteria = PriorityVector.build(matrixIndicatorIndicator);
 
-//		Consistency verification
+		/** Consistency verification */
 		if (!consistencyChecker.isConsistent(matrixIndicatorIndicator, vectorIndicatorCriteria)) {
 			Logger.getAnonymousLogger().log(Level.SEVERE,
-											"Is not consistent (criteria {0})",
-											identifier);
+				"Is not consistent (criteria {0})",
+				identifier);
 		}
-//		Initialisation of the Indicators
+		/** Initialisation of the Indicators */
 		@SuppressWarnings("unchecked")
 		final List<Element> xmlIndicatorsList = (List<Element>) xmlCriteria.getChildren("indicator");
 		@SuppressWarnings("unchecked")
 		final List<Element> xmlRowsList = (List<Element>) xmlPrefMatrix.getChildren("row");
 		indicators = new ArrayList<>(xmlIndicatorsList.size());
-//		Verification that the number of indicators matches the size of the matrix
+		/** Verification that the number of indicators matches the size of the matrix */
 		if (xmlIndicatorsList.size() != xmlRowsList.size()) {
 			Logger.getAnonymousLogger().severe(
-					"Error : the number of Indicators and the size of the preference matrix does not match !");
+				"Error : the number of Indicators and the size of the preference matrix does not match !");
 		}
-//		For each indicator declared in the configuration file
+		/** For each indicator declared in the configuration file */
 		for (Element xmlIndicator : xmlIndicatorsList) {
 			//				System.out.println("\tCriterion.xmlIndicator="+xmlIndicator);
 //				System.out.println("\tCriterion.xmlIndicator.attValue="+xmlIndicator.getAttributeValue("id"));
 			final String indicatorName = AHPRoot.indicatorPath
-					+ Indicator.class.getSimpleName()
-					+ xmlIndicator.getAttributeValue("id");
+				+ Indicator.class.getSimpleName()
+				+ xmlIndicator.getAttributeValue("id");
 			try {
-//					Research of the class implementing the indicator , named "org.taeradan.ahp.ind.IndicatorCxIy"
+				/** Research of the class implementing the indicator , named "org.taeradan.ahp.ind.IndicatorCxIy" */
 				@SuppressWarnings("unchecked")
 				final Class<? extends Indicator> indClass = (Class<? extends Indicator>) Class.forName(indicatorName);
 //					System.out.println("\t\tCriterion.indClass="+indClass);
-//					Extraction of its constructor
+				/** Extraction of its constructor */
 				final Constructor<? extends Indicator> indConstruct = indClass.getConstructor(Element.class);
 //					System.out.println("\t\tCriterion.indConstruct="+indConstruct);
-//					Instanciation of the indicator with its constructor
+				/** Instanciation of the indicator with its constructor */
 				indicators.add(indConstruct.newInstance(xmlIndicator));
 
 //					System.out.println("\tCriterion.indicator="+indicators.get(i));
@@ -108,8 +108,8 @@ public class Criterion
 				Logger.getAnonymousLogger().log(Level.SEVERE, "Error : no such constructor :{0}", e);
 			} catch (ClassNotFoundException e) {
 				Logger.getAnonymousLogger().log(Level.SEVERE,
-						"Error : class {0} not found :{1}",
-						new Object[]{indicatorName, e});
+					"Error : class {0} not found :{1}",
+					new Object[]{indicatorName, e});
 			} catch (Exception e) {
 				Logger.getAnonymousLogger().log(Level.SEVERE, "Error :{0}", e);
 			}
@@ -117,7 +117,7 @@ public class Criterion
 	}
 
 	public PriorityVector calculateAlternativesPriorityVector(
-			final Collection<? extends Alternative> alternatives) {
+		final Collection<? extends Alternative> alternatives) {
 
 		final PairWiseMatrix matrixAlternativesIndicator = new PairWiseMatrix(alternatives.size(), indicators.size());
 
@@ -126,11 +126,11 @@ public class Criterion
 		int index = 0;
 		while (indicatorIterator.hasNext()) {
 			matrixAlternativesIndicator.setMatrix(
-					0,
-					alternatives.size() - 1,
-					index,
-					index,
-					indicatorIterator.next().calculateAlternativesPriorityVector(alternatives));
+				0,
+				alternatives.size() - 1,
+				index,
+				index,
+				indicatorIterator.next().calculateAlternativesPriorityVector(alternatives));
 
 			index++;
 		}
@@ -138,19 +138,23 @@ public class Criterion
 //		Calculation of the criteria's alternatives vector
 		vectorAlternativesCriteria = new PriorityVector(matrixAlternativesIndicator.getRowDimension());
 		vectorAlternativesCriteria.setMatrix(
-				matrixAlternativesIndicator.getRowDimension() - 1,
-				matrixAlternativesIndicator.times(vectorIndicatorCriteria));
+			matrixAlternativesIndicator.getRowDimension() - 1,
+			matrixAlternativesIndicator.times(vectorIndicatorCriteria));
 
 		return vectorAlternativesCriteria;
 	}
 
-	/** @return Ca string describing the criteria, but not its children */
+	/**
+	 * @return Ca string describing the criteria, but not its children
+	 */
 	@Override
 	public String toString() {
 		return "Criterion " + identifier + " : " + name;
 	}
 
-	/** @return a string describing the criterion and all its children */
+	/**
+	 * @return a string describing the criterion and all its children
+	 */
 	public String toStringRecursive() {
 		final StringBuilder sb = new StringBuilder(this.toString());
 		sb.append("\n").append(matrixIndicatorIndicator.toString("\t"));
@@ -167,7 +171,9 @@ public class Criterion
 		return sb.toString();
 	}
 
-	/** @return a JDOM element that represents the criteria and all its children */
+	/**
+	 * @return a JDOM element that represents the criteria and all its children
+	 */
 	@Override
 	public Element toXml() {
 		final Element xmlCriteria = new Element("criteria");
