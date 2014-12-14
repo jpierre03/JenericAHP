ce3154f12d09fe4e98825f5b10228f9e3a423cbe
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2014-12-14 15:19:43 +0100

    nsp

--- libahp/sandbox.c
+++ libahp/sandbox.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct {
+	unsigned short int size;
+	double* weight;
+} globalPriority;
+
+globalPriority* buildGlobalPriorityVector(){
+	unsigned short int size = 5;
+	double weight[] = {0.2, 0.2, 0.1, 0.4, 0.2};
+
+	globalPriority* gp = malloc(sizeof(globalPriority));
+	gp->size = size;
+	gp->weight = malloc(size * sizeof(double));
+	memcpy(gp->weight, weight, size * sizeof(double)); 
+
+	return gp;
+}
+
+void displayGlobalPriority(globalPriority* gp){
+	int i = 0;
+	for(i = 0; i < gp->size; i++){
+		printf("%d %ld\n", i, (gp->weight)[i]);
+	}
+
+}
+
 
 int main(int argc, char** argv){
-	printf("Hello world !");
+	printf("Hello world !\n");
+	globalPriority* gp = buildGlobalPriorityVector();
+	displayGlobalPriority(gp);
 
 	return 0;
 }
